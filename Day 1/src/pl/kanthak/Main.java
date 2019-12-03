da4368848cca10a2aa27795ef7a51fc3b1fcf086
domino888 <kanthakdominik@gmail.com>
2019-12-03 09:52:16 +0100

    Part one completed

--- Day 1/src/pl/kanthak/Main.java
+++ Day 1/src/pl/kanthak/Main.java
@@ -1,8 +1,32 @@
 package pl.kanthak;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> massOfModules = new ArrayList<>();
+        List<Integer> neededFuels = new ArrayList<>();
+        String line;
+
+        while (!(line = scanner.nextLine()).isEmpty()) {
+            massOfModules.add(Integer.parseInt(line));
+        }
+
+        for(Integer mass : massOfModules){
+            mass = mass/3 - 2;
+            neededFuels.add(mass);
+        }
+
+        Integer fuelRequirements = neededFuels
+                                            .stream()
+                                             .reduce(0, Integer::sum);
+
+        System.out.println(fuelRequirements);
     }
 }
