9cf70f1fee2322795e6143defdb8b8efbb1dbeaf
domino888 <kanthakdominik@gmail.com>
2019-12-03 12:43:30 +0100

    Part two completed

--- Day 1/src/pl/kanthak/Main.java
+++ Day 1/src/pl/kanthak/Main.java
@@ -18,15 +18,28 @@ public class Main {
             massOfModules.add(Integer.parseInt(line));
         }
 
-        for(Integer mass : massOfModules){
-            mass = mass/3 - 2;
+        for (Integer mass : massOfModules) {
+            // mass = mass/3 - 2;            // Part ONE
+            mass = calculateFuel(mass);             // Part TWO
             neededFuels.add(mass);
         }
 
         Integer fuelRequirements = neededFuels
-                                            .stream()
-                                             .reduce(0, Integer::sum);
+                .stream()
+                .reduce(0, Integer::sum);
 
         System.out.println(fuelRequirements);
+    }
+
+    static public Integer calculateFuel(Integer mass) {
+        Integer sumOfMass = 0;
+        while (mass > 0) {
+            mass = mass / 3 - 2;
+            if(mass == -2 || mass == -1){
+                mass = 0;
+            }
+            sumOfMass += mass;
+        }
+        return sumOfMass;
     }
 }
