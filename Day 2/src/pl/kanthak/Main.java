72b366a0e382f597efdbeed086a6819489e1cc68
domino888 <kanthakdominik@gmail.com>
2019-12-03 23:49:45 +0100

    Add readLineFromConsole

--- Day 2/src/pl/kanthak/Main.java
+++ Day 2/src/pl/kanthak/Main.java
@@ -3,6 +3,27 @@ package pl.kanthak;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        List<Integer> integerList = new ArrayList<>();
+        readLineFromConsole(integerList);
+        integerList.stream()
+                .forEach(System.out::println);
+    }
+
+    public static void readLineFromConsole(List<Integer> integerList) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
+
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] lineWithoutCommas = line.trim().split(",");
+
+        for (String number : lineWithoutCommas) {
+            integerList.add(Integer.parseInt(number));
+        }
     }
 }
