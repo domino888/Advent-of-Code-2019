e2d8befce3842f9980470c55476d454a44b2d1a3
domino888 <kanthakdominik@gmail.com>
2019-12-11 01:50:31 +0100

    Day 4 stage 1 completed

--- Day 4/src/pl/kanthak/Main.java
+++ Day 4/src/pl/kanthak/Main.java
@@ -2,31 +2,49 @@ package pl.kanthak;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Character[]> passwordsList = new ArrayList<>();
+        List<List<Character>> passwordsList = new ArrayList<>();
+        List<Character> password = new ArrayList<>();
         String passwordText;
-        Character[] password;
+        boolean check = false;
+        int counter = 0;
+        int howMany = 0;
 
-
-        for (Integer i = 256310; i < 732736; i++) {
-            passwordText = i.toString();
-            password = new Character[6];
-            for (int j = 0; j < 6; j++) {
-                password[j] = passwordText.charAt(j);
+        for (int i = 256310; i < 732737; i++) {
+            passwordText = Integer.toString(i);
+            for (int j = 0; j < passwordText.length(); j++) {
+                password.add(passwordText.charAt(j));
             }
-            passwordsList.add(password);
-        }
-        for (Character[] characters : passwordsList) {
-            for (Character character : characters) {
-                System.out.print(character);
-            }
-            System.out.println();
+            passwordsList.add(new ArrayList<>(password));
+            password.clear();
         }
 
+        for (List<Character> characters : passwordsList) {
+            ListIterator<Character> listIterator = characters.listIterator();
 
+            for (int i = 0; i < characters.size(); i++) {
+                listIterator.next();
+                if (i == characters.size() - 1) {
+                    break;
+                }
+                if (characters.get(i) <= characters.get(listIterator.nextIndex())) {
+                    if (characters.get(i) == characters.get(listIterator.nextIndex())) {
+                        check = true;
+                    }
+                    counter++;
+                }
+            }
+            if (counter == 5 && check) {
+                howMany++;
+            }
+            counter = 0;
+            check = false;
+        }
+        System.out.println(howMany);
     }
 }
