c10ffd02093853473b12876c8c184767f492ec36
kanthakdominik <kanthakdominik@gmail.com>
2021-08-31 00:00:45 +0200

    added Day8 stage2

--- src/main/java/pl.dominik/Day8/Day8.java
+++ src/main/java/pl.dominik/Day8/Day8.java
@@ -1,6 +1,5 @@
 package main.java.pl.dominik.Day8;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,22 +10,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class Day8 {
+    private final int numberOfPixels = 25 * 6;
+    private final List<List<Integer>> layers = readLayers();
 
-    public void execute() throws IOException {
-        System.out.println("Day 1: ");
-        System.out.println("Part One result: " + computeImageChecksum(readNumberFromFile()));
+    public Day8() throws IOException {
     }
 
-    public int computeImageChecksum(List<Integer> integerList) {
-        int numberOfPixels = 25 * 6;
-        int numberOfLayers = integerList.size() / numberOfPixels;
-        List<List<Integer>> layers = new ArrayList<>(numberOfLayers);
-        List<Integer> searchedLayer = new ArrayList<>();
-        int minNumberOfZeros = Integer.MAX_VALUE;
+    public void execute() {
+        System.out.println("Day 8: ");
+        System.out.println("Part One result: " + computeImageChecksum());
+        System.out.println("Part Two result: ");
+        decodeImageMessage().forEach(System.out::println);
+    }
 
-        for (int i = 0; i < integerList.size(); i += numberOfPixels) {
-            layers.add(integerList.subList(i, i + numberOfPixels));
-        }
+    public int computeImageChecksum() {
+        int minNumberOfZeros = Integer.MAX_VALUE;
+        List<Integer> searchedLayer = new ArrayList<>();
 
         for (List<Integer> layer : layers) {
             int numberOfZeros = Collections.frequency(layer, 0);
@@ -38,14 +37,44 @@ public class Day8 {
         return Collections.frequency(searchedLayer, 1) * Collections.frequency(searchedLayer, 2);
     }
 
-    public List<Integer> readNumberFromFile() throws IOException {
+    public List<String> decodeImageMessage() {
+        List<Integer> imageList = new ArrayList<>(Collections.nCopies(numberOfPixels, 2));
+        List<String> message = new ArrayList<>();
+        boolean[] isSet = new boolean[numberOfPixels];
+
+        for (List<Integer> layer : layers) {
+            for (int pixel = 0; pixel < numberOfPixels; pixel++) {
+                if (!isSet[pixel]) {
+                    if (layer.get(pixel) == 0) {
+                        imageList.set(pixel, 0);
+                        isSet[pixel] = true;
+                    } else if (layer.get(pixel) == 1) {
+                        imageList.set(pixel, 1);
+                        isSet[pixel] = true;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < numberOfPixels; i += 25) {
+            message.add(imageList.subList(i, i + 25).toString().replaceAll("[\\[\\]]", "").replaceAll(",", " ").replaceAll("\\s+", "").replaceAll("0", " ").replaceAll("1", "\u2588"));
+
+        }
+        return message;
+    }
+
+    public List<List<Integer>> readLayers() throws IOException {
         Path path = Paths.get("src/main/resources/Day8/data.txt");
         BufferedReader reader = Files.newBufferedReader(path);
         String line = reader.readLine();
         List<Integer> integerList = new ArrayList<>();
+        List<List<Integer>> layers = new ArrayList<>();
+
         for (int i = 0; i < line.length(); i++) {
             integerList.add(Character.getNumericValue(line.charAt(i)));
         }
-        return integerList;
+        for (int i = 0; i < integerList.size(); i += numberOfPixels) {
+            layers.add(integerList.subList(i, i + numberOfPixels));
+        }
+        return layers;
     }
 }
--- src/test/Day8/Day8Test.java
+++ src/test/Day8/Day8Test.java
@@ -4,6 +4,7 @@ import main.java.pl.dominik.Day8.Day8;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,8 +12,22 @@ public class Day8Test {
 
     private final Day8 day8 = new Day8();
 
+    public Day8Test() throws IOException {
+    }
+
     @Test
-    void checkComputingImageChecksum() throws IOException {
-        assertEquals(2250, day8.computeImageChecksum(day8.readNumberFromFile()));
+    void checkComputingImageChecksum() {
+        assertEquals(2250, day8.computeImageChecksum());
+    }
+
+    @Test
+    void checkDecodingMessage() {
+        assertEquals(List.of(
+                "████ █  █   ██ █  █ █    ",
+                "█    █  █    █ █  █ █    ",
+                "███  ████    █ █  █ █    ",
+                "█    █  █    █ █  █ █    ",
+                "█    █  █ █  █ █  █ █    ",
+                "█    █  █  ██   ██  ████ "), day8.decodeImageMessage());
     }
 }
